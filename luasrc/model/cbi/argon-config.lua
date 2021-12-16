4c2ba4dc1903341e570d3e9d7cecf01d548b2b41
Beginner-Go <70857188+Beginner-Go@users.noreply.github.com>
2021-12-16 13:02:07 +0000

    compatible 19.07 luci

--- luasrc/model/cbi/argon-config.lua
+++ luasrc/model/cbi/argon-config.lua
@@ -50,9 +50,12 @@ local transparency_sets = {
 }
 
 -- [[ 模糊设置 ]]--
-br = SimpleForm('config', translate('Argon Config'), translate('Here you can set the blur and transparency of the login page of argon theme, and manage the background pictures and videos.[Chrome is recommended]'))
+br = SimpleForm('config')
+br.title = translate('Argon Config')
+br.description = translate('Here you can set the blur and transparency of the login page of argon theme, and manage the background pictures and videos.[Chrome is recommended]')
 br.reset = false
 br.submit = false
+
 s = br:section(SimpleSection) 
 
 o = s:option(ListValue, 'bing_background', translate('Wallpaper Source'))
@@ -62,21 +65,21 @@ o.default = bing_background
 o.rmempty = false
 
 o = s:option(ListValue, 'mode', translate('Theme mode'))
+o.description = translate('You can choose Theme color mode here')
 o:value('normal', translate('Follow System'))
 o:value('light', translate('Force Light'))
 o:value('dark', translate('Force Dark'))
 o.default = mode
 o.rmempty = false
-o.description = translate('You can choose Theme color mode here')
 
-o = s:option(Value, 'primary', translate('[Light mode] Primary Color'), translate('A HEX Color ; ( Default: #5e72e4 )'))
+o = s:option(Value, 'primary', translate('[Light mode] Primary Color'))
+o.description = translate('A HEX Color ; ( Default: #5e72e4 )')
 o.default = primary
 o.datatype = ufloat
 o.rmempty = false
 
-
-
-o = s:option(ListValue, 'transparency', translate('[Light mode] Transparency'), translate('0 transparent - 1 opaque ; ( Suggest: transparent: 0 or translucent preset: 0.5 )'))
+o = s:option(ListValue, 'transparency', translate('[Light mode] Transparency'))
+o.description = translate('0 transparent - 1 opaque ; ( Suggest: transparent: 0 or translucent preset: 0.5 )')
 for _, v in ipairs(transparency_sets) do
     o:value(v)
 end
@@ -84,17 +87,20 @@ o.default = blur_opacity
 o.datatype = ufloat
 o.rmempty = false
 
-o = s:option(Value, 'blur', translate('[Light mode] Frosted Glass Radius'), translate('Larger value will more blurred ; ( Suggest:  clear: 1 or blur preset: 10 )'))
+o = s:option(Value, 'blur', translate('[Light mode] Frosted Glass Radius'))
+o.description = translate('Larger value will more blurred ; ( Suggest:  clear: 1 or blur preset: 10 )')
 o.default = blur_radius
 o.datatype = ufloat
 o.rmempty = false
 
-o = s:option(Value, 'dark_primary', translate('[Dark mode] Primary Color'), translate('A HEX Color ; ( Default: #483d8b )'))
+o = s:option(Value, 'dark_primary', translate('[Dark mode] Primary Color'))
+o.description = translate('A HEX Color ; ( Default: #483d8b )')
 o.default = dark_primary
 o.datatype = ufloat
 o.rmempty = false
 
-o = s:option(ListValue, 'transparency_dark', translate('[Dark mode] Transparency'), translate('0 transparent - 1 opaque ; ( Suggest: Black translucent preset: 0.5 )'))
+o = s:option(ListValue, 'transparency_dark', translate('[Dark mode] Transparency'))
+o.description = translate('0 transparent - 1 opaque ; ( Suggest: Black translucent preset: 0.5 )')
 for _, v in ipairs(transparency_sets) do
     o:value(v)
 end
@@ -102,7 +108,8 @@ o.default = blur_opacity_dark
 o.datatype = ufloat
 o.rmempty = false
 
-o = s:option(Value, 'blur_dark', translate('[Dark mode] Frosted Glass Radius'), translate('Larger value will more blurred ; ( Suggest:  clear: 1 or blur preset: 10 )'))
+o = s:option(Value, 'blur_dark', translate('[Dark mode] Frosted Glass Radius'))
+o.description = translate('Larger value will more blurred ; ( Suggest:  clear: 1 or blur preset: 10 )')
 o.default = blur_radius_dark
 o.datatype = ufloat
 o.rmempty = false
@@ -121,7 +128,8 @@ function br.handle(self, state, data)
     return true
 end
 
-ful = SimpleForm('upload', translate('Upload  (Free: ') .. wa.byte_format(free_byte) .. ')', translate("You can upload files such as jpg,png,gif,mp4 files, To change the login page background."))
+ful = SimpleForm('upload', translate('Upload  (Free: ') .. wa.byte_format(free_byte) .. ')')
+ful.description = translate("You can upload files such as jpg,png,gif,mp4 files, To change the login page background.")
 ful.reset = false
 ful.submit = false
 
