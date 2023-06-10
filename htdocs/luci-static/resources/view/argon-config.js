jerrykuku/luci-app-argon-config

commit 5b8057beda83e9c48bf646a790bec65fa4a38f5e

refactor: rewritten in JS
Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>

htdocs/luci-static/resources/view/argon-config.js
@@ -1,0 +1,186 @@
+'use strict';
+'require form';
+'require fs';
+'require rpc';
+'require uci';
+'require ui';
+'require view';
+
+var callSystemInfo = rpc.declare({
+	object: 'system',
+	method: 'info'
+});
+
+var callRemoveArgon = rpc.declare({
+	object: 'luci.argon',
+	method: 'remove',
+	params: ['filename'],
+	expect: { '': {} }
+});
+
+var callRenameArgon = rpc.declare({
+	object: 'luci.argon',
+	method: 'rename',
+	params: ['newname'],
+	expect: { '': {} }
+});
+
+var bg_path = '/www/luci-static/argon/background/';
+
+var trans_set = [0, 0.1, 0.2, 0.3, 0.4,
+	0.5, 0.6, 0.7, 0.8, 0.9, 1 ];
+
+return view.extend({
+	load: function() {
+		return Promise.all([
+			uci.load('argon'),
+			L.resolveDefault(callSystemInfo(), {}),
+			L.resolveDefault(fs.list(bg_path), {})
+		]);
+	},
+
+	render: function(data) {
+		var m, s, o;
+
+		m = new form.Map('argon', _('Argon theme configuration'),
+			_('Here you can set the blur and transparency of the login page of argon theme, and manage the background pictures and videos. Chrome is recommended.'));
+
+		s = m.section(form.TypedSection, 'global', _('Theme configuration'));
+		s.addremove = false;
+		s.anonymous = true;
+
+		o = s.option(form.ListValue, 'online_wallpaper', _('Wallpaper source'));
+		o.value('none', _('Built-in'));
+		o.value('bing', _('Bing'));
+		o.value('unsplash', _('Unsplash'));
+		o.value('wallhaven', _('Wallhaven'));
+		o.default = 'bing';
+		o.rmempty = false;
+
+		o = s.option(form.ListValue, 'mode', _('Theme mode'));
+		o.value('normal', _('Follow system'));
+		o.value('light', _('Light mode'));
+		o.value('dark', _('Dark mode'));
+		o.default = 'normal';
+		o.rmempty = false;
+
+		o = s.option(form.Value, 'primary', _('[Light mode] Primary Color'), _('A HEX color (default: #5e72e4).'))
+		o.default = '#5e72e4';
+		o.rmempty = false;
+		o.validate = function(section_id, value) {
+			if (section_id)
+				return /(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i.test(value) ||
+					_('Expecting: %s').format(_('valid HEX color value'));
+			return true;
+		}
+
+		o = s.option(form.ListValue, 'transparency', _('[Light mode] Transparency'),
+			_('0 transparent - 1 opaque (suggest: transparent: 0 or translucent preset: 0.5).'));
+		for (var i of trans_set)
+			o.value(i);
+		o.default = '0.5';
+		o.rmempty = false;
+
+		o = s.option(form.Value, 'blur', _('[Light mode] Frosted Glass Radius'),
+			_('Larger value will more blurred (suggest: clear: 1 or blur preset: 10).'));
+		o.datatype = 'ufloat';
+		o.default = '10';
+		o.rmempty = false;
+
+		o = s.option(form.Value, 'dark_primary', _('[Dark mode] Primary Color'),
+			_('A HEX Color (default: #483d8b).'))
+		o.default = '#483d8b';
+		o.rmempty = false;
+		o.validate = function(section_id, value) {
+			if (section_id)
+				return /(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i.test(value) ||
+					_('Expecting: %s').format(_('valid HEX color value'));
+			return true;
+		}
+
+		o = s.option(form.ListValue, 'transparency_dark', _('[Dark mode] Transparency'),
+			_('0 transparent - 1 opaque (suggest: black translucent preset: 0.5).'));
+		for (var i of trans_set)
+			o.value(i);
+		o.default = '0.5';
+		o.rmempty = false;
+
+		o = s.option(form.Value, 'blur_dark', _('[Dark mode] Frosted Glass Radius'),
+			_('Larger value will more blurred (suggest: clear: 1 or blur preset: 10).'))
+		o.datatype = 'ufloat';
+		o.default = '10';
+		o.rmempty = false;
+
+		o = s.option(form.Button, '_save', _('Save settings'));
+		o.inputstyle = 'apply';
+		o.inputtitle = _('Save current settings');
+		o.onclick = function() {
+			ui.changes.apply(true);
+			return this.map.save(null, true);
+		}
+
+		s = m.section(form.TypedSection, null, _('Upload background (available space: %1024.2mB)')
+			.format(data[1].root.avail * 1024),
+			_('You can upload files such as gif/jpg/mp4/png/webm/webp files, to change the login page background.'));
+		s.addremove = false;
+		s.anonymous = true;
+
+		o = s.option(form.Button, '_upload_bg', _('Upload background'),
+			_('Files will be uploaded to <code>%s</code>.').format(bg_path));
+		o.inputstyle = 'action';
+		o.inputtitle = _('Upload...');
+		o.onclick = function(ev, section_id) {
+			var file = '/tmp/argon_background.tmp';
+			return ui.uploadFile(file, ev.target).then(function(res) {
+				return L.resolveDefault(callRenameArgon(res.name), {}).then(function(ret) {
+					if (ret.result === 0)
+						return location.reload();
+					else {
+						ui.addNotification(null, E('p', _('Failed to upload file: %s.').format(res.name)));
+						return L.resolveDefault(fs.remove(file), {});
+					}
+				});
+			})
+			.catch(function(e) { ui.addNotification(null, E('p', e.message)); });
+		};
+		o.modalonly = true;
+
+		s = m.section(form.TableSection);
+		s.render = function() {
+			var tbl = E('table', { 'class': 'table cbi-section-table' },
+				E('tr', { 'class': 'tr table-titles' }, [
+					E('th', { 'class': 'th' }, [ _('Filename') ]),
+					E('th', { 'class': 'th' }, [ _('Modified date') ]),
+					E('th', { 'class': 'th' }, [ _('Size') ]),
+					E('th', { 'class': 'th' }, [ _('Action') ])
+				])
+			);
+
+			cbi_update_table(tbl, data[2].map(L.bind(function(file) {
+				return [
+					file.name,
+					new Date(file.mtime * 1000).toLocaleString(),
+					String.format('%1024.2mB', file.size),
+					E('button', {
+						'class': 'btn cbi-button cbi-button-remove',
+						'click': ui.createHandlerFn(this, function() {
+							return L.resolveDefault(callRemoveArgon(file.name), {})
+							.then(function() { return location.reload(); });
+						})
+					}, [ _('Delete') ])
+				];
+			}, this)), E('em', _('No files found.')));
+
+			return E('div', { 'class': 'cbi-map', 'id': 'cbi-filelist' }, [
+				E('h3', _('Background file list')),
+				tbl
+			]);
+		};
+
+		return m.render();
+	},
+
+	handleSaveApply: null,
+	handleSave: null,
+	handleReset: null
+});

root/etc/config/argon
@@ -1,9 +1,10 @@
 config global
 	option primary '#5e72e4'
 	option dark_primary '#483d8b'
-	option blur '10'
-	option blur_dark '10'
-	option transparency '0.5'
-	option transparency_dark '0.5'
+	option blur '0'
+	option blur_dark '0'
+	option transparency '0.3'
+	option transparency_dark '0.3'
 	option mode 'normal'
-	option bing_background '0'
+	option online_wallpaper 'bing'
+

root/usr/libexec/rpcd/luci.argon
@@ -1,0 +1,56 @@
+#!/bin/sh
+
+. /lib/functions.sh
+. /usr/share/libubox/jshn.sh
+
+readonly bg_path="/www/luci-static/argon/background"
+readonly tmp_path="/tmp/argon_background.tmp"
+
+case "$1" in
+"list")
+	json_init
+	json_add_object "remove"
+		json_add_string "filename" "filename"
+	json_close_object
+	json_add_object "rename"
+		json_add_string "newname" "filename"
+	json_close_object
+	json_dump
+	json_cleanup
+	;;
+"call")
+	case "$2" in
+	"remove")
+		read -r input
+		json_load "$input"
+		json_get_var filename "filename"
+		json_cleanup
+
+		if dirname "$filename" | grep -q ".."; then
+			echo '{ "result": 255 }'
+			exit 255
+		fi
+
+		rm -f "$bg_path/$filename"
+		echo '{ "result": 0 }'
+		;;
+	"rename")
+		read -r input
+		json_load "$input"
+		json_get_var newname "newname"
+		json_cleanup
+
+		if dirname "$newname" | grep -q ".."; then
+			echo '{ "result": 255 }'
+			exit 255
+		fi
+
+		if mv "$tmp_path" "$bg_path/$newname" 2>"/dev/null"; then
+			echo '{ "result": 0 }'
+		else
+			echo '{ "result": 1 }'
+		fi
+		;;
+	esac
+	;;
+esac

Makefile
@@ -1,14 +1,14 @@
 include $(TOPDIR)/rules.mk
 
 PKG_NAME:=luci-app-argon-config
-PKG_VERSION:=0.9
-PKG_RELEASE:=20220424
+PKG_VERSION:=1.0
+PKG_RELEASE:=20230608
 
 PKG_MAINTAINER:=jerrykuku <jerrykuku@qq.com>
 
-LUCI_TITLE:=LuCI page for Argon Config
+LUCI_TITLE:=LuCI app for Argon theme configuration
 LUCI_PKGARCH:=all
-LUCI_DEPENDS:=+luci-compat +luci-lib-ipkg +luci-theme-argon
+LUCI_DEPENDS:=+luci-theme-argon
 
 define Package/$(PKG_NAME)/conffiles
 /etc/config/argon

luasrc/controller/argon-config.lua
@@ -1,10 +1,0 @@
-module("luci.controller.argon-config", package.seeall)
-
-function index()
-	if not nixio.fs.access('/www/luci-static/argon/css/cascade.css') then
-		return
-	end
-
-	local page = entry({"admin", "system", "argon-config"}, form("argon-config"), _("Argon Config"), 90)
-	page.acl_depends = { "luci-app-argon-config" }
-end

luasrc/model/cbi/argon-config.lua
@@ -1,217 +1,0 @@
-local nxfs = require 'nixio.fs'
-local wa = require 'luci.tools.webadmin'
-local opkg = require 'luci.model.ipkg'
-local sys = require 'luci.sys'
-local http = require 'luci.http'
-local nutil = require 'nixio.util'
-local name = 'argon'
-local uci = require 'luci.model.uci'.cursor()
-
-local fstat = nxfs.statvfs(opkg.overlay_root())
-local space_total = fstat and fstat.blocks or 0
-local space_free = fstat and fstat.bfree or 0
-local space_used = space_total - space_free
-
-local free_byte = space_free * fstat.frsize
-
-local primary, dark_primary, blur_radius, blur_radius_dark, blur_opacity, mode
-if nxfs.access('/etc/config/argon') then
-	primary = uci:get_first('argon', 'global', 'primary')
-	dark_primary = uci:get_first('argon', 'global', 'dark_primary')
-	blur_radius = uci:get_first('argon', 'global', 'blur')
-	blur_radius_dark = uci:get_first('argon', 'global', 'blur_dark')
-	blur_opacity = uci:get_first('argon', 'global', 'transparency')
-	blur_opacity_dark = uci:get_first('argon', 'global', 'transparency_dark')
-	mode = uci:get_first('argon', 'global', 'mode')
-	bing_background = uci:get_first('argon', 'global', 'bing_background')
-end
-
-function glob(...)
-    local iter, code, msg = nxfs.glob(...)
-    if iter then
-        return nutil.consume(iter)
-    else
-        return nil, code, msg
-    end
-end
-
-local transparency_sets = {
-    0,
-    0.1,
-    0.2,
-    0.3,
-    0.4,
-    0.5,
-    0.6,
-    0.7,
-    0.8,
-    0.9,
-    1
-}
-
--- [[ 模糊设置 ]]--
-br = SimpleForm('config', translate('Argon Config'), translate('Here you can set the blur and transparency of the login page of argon theme, and manage the background pictures and videos.[Chrome is recommended]'))
-br.reset = false
-br.submit = false
-s = br:section(SimpleSection) 
-
-o = s:option(ListValue, 'bing_background', translate('Wallpaper Source'))
-o:value('0', translate('Built-in'))
-o:value('1', translate('Bing Wallpapers'))
-o.default = bing_background
-o.rmempty = false
-
-o = s:option(ListValue, 'mode', translate('Theme mode'))
-o:value('normal', translate('Follow System'))
-o:value('light', translate('Force Light'))
-o:value('dark', translate('Force Dark'))
-o.default = mode
-o.rmempty = false
-o.description = translate('You can choose Theme color mode here')
-
-o = s:option(Value, 'primary', translate('[Light mode] Primary Color'), translate('A HEX Color ; ( Default: #5e72e4 )'))
-o.default = primary
-o.datatype = ufloat
-o.rmempty = false
-
-
-
-o = s:option(ListValue, 'transparency', translate('[Light mode] Transparency'), translate('0 transparent - 1 opaque ; ( Suggest: transparent: 0 or translucent preset: 0.5 )'))
-for _, v in ipairs(transparency_sets) do
-    o:value(v)
-end
-o.default = blur_opacity
-o.datatype = ufloat
-o.rmempty = false
-
-o = s:option(Value, 'blur', translate('[Light mode] Frosted Glass Radius'), translate('Larger value will more blurred ; ( Suggest:  clear: 1 or blur preset: 10 )'))
-o.default = blur_radius
-o.datatype = ufloat
-o.rmempty = false
-
-o = s:option(Value, 'dark_primary', translate('[Dark mode] Primary Color'), translate('A HEX Color ; ( Default: #483d8b )'))
-o.default = dark_primary
-o.datatype = ufloat
-o.rmempty = false
-
-o = s:option(ListValue, 'transparency_dark', translate('[Dark mode] Transparency'), translate('0 transparent - 1 opaque ; ( Suggest: Black translucent preset: 0.5 )'))
-for _, v in ipairs(transparency_sets) do
-    o:value(v)
-end
-o.default = blur_opacity_dark
-o.datatype = ufloat
-o.rmempty = false
-
-o = s:option(Value, 'blur_dark', translate('[Dark mode] Frosted Glass Radius'), translate('Larger value will more blurred ; ( Suggest:  clear: 1 or blur preset: 10 )'))
-o.default = blur_radius_dark
-o.datatype = ufloat
-o.rmempty = false
-
-o = s:option(Button, 'save', translate('Save Changes'))
-o.inputstyle = 'reload'
-
-function br.handle(self, state, data)
-    if (state == FORM_VALID and data.blur ~= nil and data.blur_dark ~= nil and data.transparency ~= nil and data.transparency_dark ~= nil and data.mode ~= nil) then
-        nxfs.writefile('/tmp/aaa', data)
-        for key, value in pairs(data) do
-            uci:set('argon','@global[0]',key,value)
-        end 
-        uci:commit('argon')
-    end
-    return true
-end
-
-ful = SimpleForm('upload', translate('Upload  (Free: ') .. wa.byte_format(free_byte) .. ')', translate("You can upload files such as jpg,png,gif,webp,mp4,webm files, To change the login page background."))
-ful.reset = false
-ful.submit = false
-
-sul = ful:section(SimpleSection, '', translate("Upload file to '/www/luci-static/argon/background/'"))
-fu = sul:option(FileUpload, '')
-fu.template = 'argon-config/other_upload'
-um = sul:option(DummyValue, '', nil)
-um.template = 'argon-config/other_dvalue'
-
-local dir, fd
-dir = '/www/luci-static/argon/background/'
-nxfs.mkdir(dir)
-http.setfilehandler(
-    function(meta, chunk, eof)
-        if not fd then
-            if not meta then
-                return
-            end
-
-            if meta and chunk then
-                fd = nixio.open(dir .. meta.file, 'w')
-            end
-
-            if not fd then
-                um.value = translate('Create upload file error.')
-                return
-            end
-        end
-        if chunk and fd then
-            fd:write(chunk)
-        end
-        if eof and fd then
-            fd:close()
-            fd = nil
-            um.value = translate('File saved to') .. ' "/www/luci-static/argon/background/' .. meta.file .. '"'
-        end
-    end
-)
-
-if http.formvalue('upload') then
-    local f = http.formvalue('ulfile')
-    if #f <= 0 then
-        um.value = translate('No specify upload file.')
-    end
-end
-
-local function getSizeStr(size)
-    local i = 0
-    local byteUnits = {' kB', ' MB', ' GB', ' TB'}
-    repeat
-        size = size / 1024
-        i = i + 1
-    until (size <= 1024)
-    return string.format('%.1f', size) .. byteUnits[i]
-end
-
-local inits, attr = {}
-for i, f in ipairs(glob(dir .. '*')) do
-    attr = nxfs.stat(f)
-    if attr then
-        inits[i] = {}
-        inits[i].name = nxfs.basename(f)
-        inits[i].mtime = os.date('%Y-%m-%d %H:%M:%S', attr.mtime)
-        inits[i].modestr = attr.modestr
-        inits[i].size = getSizeStr(attr.size)
-        inits[i].remove = 0
-        inits[i].install = false
-    end
-end
-
-form = SimpleForm('filelist', translate('Background file list'), nil)
-form.reset = false
-form.submit = false
-
-tb = form:section(Table, inits)
-nm = tb:option(DummyValue, 'name', translate('File name'))
-mt = tb:option(DummyValue, 'mtime', translate('Modify time'))
-sz = tb:option(DummyValue, 'size', translate('Size'))
-btnrm = tb:option(Button, 'remove', translate('Remove'))
-btnrm.render = function(self, section, scope)
-    self.inputstyle = 'remove'
-    Button.render(self, section, scope)
-end
-
-btnrm.write = function(self, section)
-    local v = nxfs.unlink(dir .. nxfs.basename(inits[section].name))
-    if v then
-        table.remove(inits, section)
-    end
-    return v
-end
-
-return br, ful, form

luasrc/view/argon-config/other_button.htm
@@ -1,7 +1,0 @@
-<%+cbi/valueheader%>
-	<% if self:cfgvalue(section) ~= false then %>
-		<input class="cbi-button cbi-input-<%=self.inputstyle or "button" %>" style="display: <%= display %>" type="submit"<%= attr("name", cbid) .. attr("id", cbid) .. attr("value", self.inputtitle or self.title)%> />
-	<% else %>
-		-
-	<% end %>
-<%+cbi/valuefooter%>

luasrc/view/argon-config/other_dvalue.htm
@@ -1,8 +1,0 @@
-<%+cbi/valueheader%>
-<span style="color: red">
-<%
-	local val = self:cfgvalue(section) or self.default or ""
-	write(pcdata(val))
-%>
-</span>
-<%+cbi/valuefooter%>

commit bd440b9e797bf0c1ffcff1f9f6f7da9d2d29ee66

fix compatibility issue with openwrt 21.02-
On OpenWrt 21.02 and lower version, "system info" doesn't provide
space info, which fails page loading. Let's simply add a new func
to rpcd script to fetch available space.

While at it, revert unnecessary changes to argon uci config.

Fixes: 5b8057beda83e9 ("refactor: rewritten in JS")

Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>

htdocs/luci-static/resources/view/argon-config.js
@@ -6,9 +6,9 @@
 'require ui';
 'require view';
 
-var callSystemInfo = rpc.declare({
-	object: 'system',
-	method: 'info'
+var callAvailSpace = rpc.declare({
+	object: 'luci.argon',
+	method: 'avail'
 });
 
 var callRemoveArgon = rpc.declare({
@@ -34,7 +34,7 @@ return view.extend({
 	load: function() {
 		return Promise.all([
 			uci.load('argon'),
-			L.resolveDefault(callSystemInfo(), {}),
+			L.resolveDefault(callAvailSpace(), {}),
 			L.resolveDefault(fs.list(bg_path), {})
 		]);
 	},
@@ -120,7 +120,7 @@ return view.extend({
 		}
 
 		s = m.section(form.TypedSection, null, _('Upload background (available space: %1024.2mB)')
-			.format(data[1].root.avail * 1024),
+			.format(data[1].avail * 1024),
 			_('You can upload files such as gif/jpg/mp4/png/webm/webp files, to change the login page background.'));
 		s.addremove = false;
 		s.anonymous = true;

root/etc/config/argon
@@ -1,10 +1,10 @@
 config global
 	option primary '#5e72e4'
 	option dark_primary '#483d8b'
-	option blur '0'
-	option blur_dark '0'
-	option transparency '0.3'
-	option transparency_dark '0.3'
+	option blur '10'
+	option blur_dark '10'
+	option transparency '0.5'
+	option transparency_dark '0.5'
 	option mode 'normal'
 	option online_wallpaper 'bing'
 

root/usr/libexec/rpcd/luci.argon
@@ -9,6 +9,8 @@ readonly tmp_path="/tmp/argon_background.tmp"
 case "$1" in
 "list")
 	json_init
+	json_add_object "avail"
+	json_close_object
 	json_add_object "remove"
 		json_add_string "filename" "filename"
 	json_close_object
@@ -20,6 +22,12 @@ case "$1" in
 	;;
 "call")
 	case "$2" in
+	"avail")
+		json_init
+		json_add_int "avail" "$(df | grep -E '/$' | awk '{print $4}')"
+		json_dump
+		json_cleanup
+		;;
 	"remove")
 		read -r input
 		json_load "$input"