3f86ee5d81182e273f0ecd975c9374346277b935
Nils Andreas Svee <me@lochnair.net>
2025-07-14 21:13:37 +0700

    chore: refactors CI/CD workflows for package builds
    Modernizes and streamlines the CI/CD workflows by using a matrix strategy to build both apk and ipk packages for different OpenWrt versions.

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
+PKG_PATH="$(readlink -f .)"
+
+cd /builder
 sed -i 's/git\.openwrt\.org\/project\/luci/github\.com\/openwrt\/luci/g' ./feeds.conf.default
 ./scripts/feeds update luci
 ./scripts/feeds install luci
-mv ./bin/luci-app-argon-config ./package/
+ln -sv "$PKG_PATH" "./package/"
 make defconfig
 make package/luci-app-argon-config/compile V=s -j$(nproc) BUILD_LOG=1
 
